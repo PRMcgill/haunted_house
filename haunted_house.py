625d618f848523222e3f03d55b7e2c87179f44e1
Patrick <mcgill.patrick2192@gmail.com>
2019-02-07 15:55:56 -0500

    finished the project

--- haunted_house.py
+++ haunted_house.py
@@ -1,7 +1,7 @@
 def prompt():
     print('What door do you choose? \n'
         'Door 1 \n'
-         'Door 2 \n'
+        'Door 2 \n'
         'Door 3 \n'
         'Door 4 \n'
         'Door 5 \n'
@@ -18,18 +18,22 @@ def make_choice():
     doors = int(options)
 
     if doors > 11 or doors < 1:
-        print('You ran into the wall. Pick a door.')
+        print('You ran into the wall. Pick a door. \n')
     elif doors % 2 == 0:
-        print('Boo! You have been scared to death by a ' + random.choice(monster))
+        print('Boo! You have been scared to death by a ' + random.choice(monster) + '\n')
     else:
-        print('You have made it through the House! You have been spared!')
+        print('You have made it through the House! You have been spared!\n')
 
 import random
 
-print('You run into a haunted house and there are 11 doors.')
+print('You run into a haunted house and there are 11 doors.\n')
 
-while True:
+running = True
+while running:
     prompt()
-    options = input('Pick a door number to walk through: ')
+    options = input('Pick a door number to walk through: \n')
     make_choice()
-    input('Continue?')
+    choice = input('Continue? Yes or No? \n')
+    if choice == 'No':
+        running = False
+
