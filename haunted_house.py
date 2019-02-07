1e65b05f8c3ee128f4158e70ea236679513fddef
Ashley Carmichael Whitt <ashley_marlaine@Ashleys-MacBook-Air.local>
2019-02-07 15:07:10 -0500

    I added a little code.

--- haunted_house.py
+++ haunted_house.py
@@ -1,34 +1,31 @@
 
-def prompt():
-    print('You run into a haunted house and there are 11 doors. \n'
-          'What door do you choose? \n'
-          'Door 1 \n'
-          'Door 2 \n'
-          'Door 3 \n'
-          'Door 4 \n'
-          'Door 5 \n'
-          'Door 6 \n'
-          'Door 7 \n'
-          'Door 8 \n'
-          'Door 9 \n'
-          'Door 10 \n'
-          'Door 11 \n'
-          )
+print('You run into a haunted house and there are 11 doors. \n'
+        'What door do you choose? \n'
+        'Door 1 \n'
+        'Door 2 \n'
+        'Door 3 \n'
+        'Door 4 \n'
+        'Door 5 \n'
+        'Door 6 \n'
+        'Door 7 \n'
+        'Door 8 \n'
+        'Door 9 \n'
+        'Door 10 \n'
+        'Door 11 \n'
+        )
 
-options = input('Pick a door number to walk through: ')
+
 
 import random
 
 monster = ['Ghoul', 'Ghost', 'Cyclop', 'Zombie']
-
+options = input('Pick a door number to walk through: ')
 doors = int(options)
-prompt()
-    for pathing in options:
-        if doors == 2:
-         print('Boo! You have been scared to death by a ' + random.choice(monster))
-        elif doors == 2:
-            print('You have made it through the House! You have been spared!')
-        else:
-            print('You ran into the wall. Good job. Pick a door')
-        break
+
+if doors > 11 or doors < 1:
+    print('You ran into the wall. Good job. Pick a door.')
+    if doors % 2 == 0:
+        print('Boo! You have been scared to death by a ' + random.choice(monster))
+    else doors == 2:
+        print('You have made it through the House! You have been spared!')
 
